e0cc6895ed10874f20c0e911d5f4af801e6990ea
Carden Z <ephemere@protonmail.com>
2019-03-26 23:56:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -27,6 +27,8 @@ class	CashRegister
   end
 
   def void_last_transaction
-
+    item = self.transactions.keys.last
+    price = transactions[item][0]
+    quantity = transactions[item][1]
   end
 end
