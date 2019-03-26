f48e81cbfe06d918220bdc8efdf3fea5920fc957
Carden Z <ephemere@protonmail.com>
2019-03-26 23:29:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,8 +13,8 @@ class	CashRegister
 
   def apply_discount
     unless discount == 0
-      disc = discount/100
-      # binding.pry
+      percentage = discount/100
+      binding.pry
       self.total = self.total * disc
     end
   end
