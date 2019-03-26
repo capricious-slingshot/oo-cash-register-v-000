254205d6db5d3c50ac2054c0412be4221634189a
Carden Z <ephemere@protonmail.com>
2019-03-26 23:40:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,8 @@ class	CashRegister
 
   def add_item(title, price, quantity=1)
     self.total += price * quantity
-    self.items << title
+    quantity.times {|x| self.items << title}
+    
   end
 
   def apply_discount
