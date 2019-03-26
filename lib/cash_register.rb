050ccf245ed4c8b24beac77e149e1fad80637ce6
Carden Z <ephemere@protonmail.com>
2019-03-26 21:42:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
 
+class	CashRegister
+  attr_accessor :discount, :total
+
+  def initialize(discount=nil)
+  	@total = 0
+		@discount = discount
+  end
+
+  def add_item(title, price)
+    self.total += price
+  end
+end
